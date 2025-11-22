538c26cb2975e38a70e2718bc9b2d51c749315db
Piyush Jha <piyushjha910@gmail.com>
2025-11-22 22:31:07 +0530

    started a new project

---
+++ project-4/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div>
+      <h1 className='text-5xl text-violet-500 font-general'>this is great font</h1>
+    </div>
+  )
+}
+
+export default App
